01dcccba5dcd9f310dd728dc55b7da66d6f63aa2
Kabdesh Alisher <wshongcai@gmail.com>
2022-06-10 00:46:39 +0600

    #2 Creating "Task" entitу.

---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public enum Status 
+    {
+        ToDo,
+        InProgress,
+        Done
+    }
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        public Status Status { get; set; }
+        
+        public int Priority { get; set; }
+        
+        public DateTime CreateDate { get; set; } 
+        public DateTime? InProgressDate { get; set; }
+        public DateTime? DoneDate { get; set; }     
+    }
+}
